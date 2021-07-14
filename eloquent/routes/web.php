4770cf13a3fcd9f0e698e65d12d58fdb7165b8cc
Daniel Alexander Muelas Rivera <danielmuelasrivera483@gmail.com>
2021-07-13 21:55:53 -0500

    Serializacion y colecciones

--- eloquent/routes/web.php
+++ eloquent/routes/web.php
@@ -46,3 +46,34 @@ Route::get('users', function () {
         {$user->posts->count()} post <br>";
     }
 });
+
+Route::get('colletions', function () {
+    $users = User::all();
+    //Listado de todos los usuarios
+    //dd($users);
+
+    //Listar un numero determinado de usuarios de la coleccion
+    //dd($users->contains(4));
+
+    //Ignorar una serie determinada de ususarios
+    //dd($users->except([1,2,3]));
+
+    //Solo muestreme el numero 4
+    //dd($users->only(4));
+
+    //Relacion con la tabla de los posts
+    //Teniendo en cuenta la relación que se realizo en:
+    //User.php
+    dd($users->load('posts'));
+});
+
+Route::get('serialization', function () {
+    $users = User::all();
+
+    //devolver como array
+    // dd($users->toArray());
+
+    $user = $users->find(1);
+    //dd($user);
+    dd($user->toJson());
+});
