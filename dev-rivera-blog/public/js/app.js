72c09184395c3271aa3d254d7a1656d3f4ce902a
Daniel Alexander Muelas Rivera <danielmuelasrivera483@gmail.com>
2022-05-07 19:57:07 -0500

    Cambios en el app.js del blog

--- dev-rivera-blog/public/js/app.js
+++ dev-rivera-blog/public/js/app.js
@@ -37410,8 +37410,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/drivera/projects/laravel-projects-course/dev-rivera-blog/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/drivera/projects/laravel-projects-course/dev-rivera-blog/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/danmcode/laravel-projects-course/dev-rivera-blog/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/danmcode/laravel-projects-course/dev-rivera-blog/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
