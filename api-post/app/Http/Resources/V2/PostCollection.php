32d8b39548112e9e912184f4ab7de7beb6e22980
Daniel Alexander Muelas Rivera <danielmuelasrivera483@gmail.com>
2022-06-04 11:13:36 -0500

    Fin de la version 2 1. Creación de colección y recurso (Solo se pueden crear estas dos) 2. Planificación de las versiones 3. Configuración de los recursos (Post). 4. Creación de la relación de un post con un usuario. 5. Planificación de las colecciones 6. Colección afecta muchos recursos al mismo tiempo, mientras que un recurso no.

---
+++ api-post/app/Http/Resources/V2/PostCollection.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\V2;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class PostCollection extends ResourceCollection
+{
+
+    //Decir que vamos a retornar con el formato de recursos
+    public $collects = PostResource::class;
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection,
+            'meta' => [
+                'organization' => 'Platzi',
+                'authors' => [
+                    'italomoralesf',
+                    'Platzi',
+                ],
+                'type' => 'articles'
+            ],
+        ];
+    }
+}
